05bccff679ccf529276f8e3dfd43ff45953ad5e4
AliPedram Karevan <pedram.karevan@gmail.com>
2021-11-28 01:11:39 +0300

    Initial commit

---
+++ src/main/java/com/pedramkarevan/mytraining/Client.java
@@ -0,0 +1,43 @@
+package com.pedramkarevan.mytraining;
+
+import java.time.LocalDate;
+
+//This is our client which also works as "director"
+public class Client {
+
+	public static void main(String[] args) {
+		User user = createUser();
+		UserDTOBuilder builder = new UserWebDTOBuilder();
+		//Client has to provide director with concrete builder
+		UserDTO dto = directBuild(builder, user);
+		System.out.println(dto);
+	}
+	
+	/**
+	 * This method serves the role of director in builder pattern. 
+	 */
+	private static UserDTO directBuild(UserDTOBuilder builder, User user) {
+		return builder.withFirstName(user.getFirstName()).withLastName(user.getLastName())
+			   .withAddress(user.getAddress())
+			   .withBirthday(user.getBirthday())
+			   .build();
+	}
+	
+	/**
+	 * Returns a sample user. 
+	 */
+	public static User createUser() {
+		User user = new User();
+		user.setBirthday(LocalDate.of(1979, 07, 6));
+		user.setFirstName("Pedram");
+		user.setLastName("Karevan");
+		Address address = new Address();
+		address.setHouseNumber("100");
+		address.setStreet("State Street");
+		address.setCity("Tehran");
+		address.setState("Iran");
+		address.setZipcode("2375");
+		user.setAddress(address);
+		return user;
+	}
+}
